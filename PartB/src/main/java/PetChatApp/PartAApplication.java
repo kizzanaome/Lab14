d51394fcdd3bd06f619fe6617fd7c6c34048df23
kizzanaome <naomekizzan@gmail.com>
2025-08-08 09:07:17 -0500

    Lab14

---
+++ PartB/src/main/java/PetChatApp/PartAApplication.java
@@ -0,0 +1,17 @@
+package PetChatApp;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PartAApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartAApplication.class, args);
+    }
+
+}
+

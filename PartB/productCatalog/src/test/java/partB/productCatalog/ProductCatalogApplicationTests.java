51c08b940bbf810caba9ebd20d1b5d6e31439d4c
kizzanaome <naomekizzan@gmail.com>
2025-08-08 20:56:34 -0500

    Lab14

---
+++ PartB/productCatalog/src/test/java/partB/productCatalog/ProductCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package partB.productCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ petHealthApp/src/test/java/petHealthApp/PetHealthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package petHealthApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetHealthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
